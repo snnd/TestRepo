0e50deca1ae850b253fd5306471353eae1c0af9d
sana <sana.niroomand@gmail.com>
2023-12-03 08:22:15 +0330

    adding even/odd number detector

--- program.c
+++ program.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-	printf("don't freak out. this is a test dude.");
+	printf("don't freak out. this is a test dude.\n");
+	int n;
+	scanf("now, hah, give a number plz: %d\n", &n);
+	if (n % 2 == 0) printf("EVEN");
+	else printf("ODD");
+	
 	return 0;
 }
